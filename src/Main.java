7850a3ec09e71d091544843b307b74f87e17c236
Aman Mittal <amanmittal3305@gmail.com>
2023-05-21 13:24:36 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+
+public class Main {
+    public static String toUppercase(String str){
+        StringBuilder sb =  new StringBuilder("");
+        //firstly we will add first letter of string in capital form
+        char ch = str.charAt(0);
+        sb.append(Character.toUpperCase(ch));
+        //loop for elements from index 1
+        for(int i = 1; i<str.length(); i++){
+            if(str.charAt(i) == ' ' && i <str.length()-1){
+                sb.append(str.charAt(i));
+                //space is printed , now we will add next letter in capital form
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            //if above condition is not true then we will go with else statement
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        //now it's time to get our answer
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "this code is written by aman mittal.";
+        System.out.println(toUppercase(str));
+    }
+}
